246302aec79d55680a35ab006d676be25ebcf28f
Changyue HU <66393857+Changyue-Hu@users.noreply.github.com>
2020-06-08 15:37:57 -0500

    R code

--- RetirementPlan/server.R
+++ RetirementPlan/server.R
@@ -9,12 +9,120 @@ shinyServer(function(input, output,session){
     # jump pages
     observeEvent(input$jump_bank_account,  {
         updateTabItems(session, "tabs", "asset")
+        updateTabsetPanel(session = session, inputId = "asset_tabs", selected = "bank_tab")
+    })
+    
+    observeEvent(input$jump_investment,  {
+        updateTabItems(session, "tabs", "asset")
+        updateTabsetPanel(session = session, inputId = "asset_tabs", selected = "investment_tab")
+    })
+    
+    observeEvent(input$jump_vehicle,  {
+        updateTabItems(session, "tabs", "asset")
+        updateTabsetPanel(session = session, inputId = "asset_tabs", selected = "vehicle_tab")
+    })
+    
+    observeEvent(input$jump_house,  {
+        updateTabItems(session, "tabs", "asset")
+        updateTabsetPanel(session = session, inputId = "asset_tabs", selected = "house_tab")
+    })
+    
+    observeEvent(input$jump_pension,  {
+        updateTabItems(session, "tabs", "asset")
+        updateTabsetPanel(session = session, inputId = "asset_tabs", selected = "pension_tab")
+    })
+    
+    observeEvent(input$jump_equity,  {
+        updateTabItems(session, "tabs", "asset")
+        updateTabsetPanel(session = session, inputId = "asset_tabs", selected = "equity_tab")
+    })
+    
+    
+    observeEvent(input$jump_otherasset,  {
+        updateTabItems(session, "tabs", "asset")
+        updateTabsetPanel(session = session, inputId = "asset_tabs", selected = "otherasset_tab")
     })
     
     observeEvent(input$jump_credit_card,  {
         updateTabItems(session, "tabs", "ex_liab")
+        updateTabsetPanel(session = session, inputId = "ex_liab_tabs", selected = "credit_card_tab")
     })
     
+    observeEvent(input$jump_loan,  {
+        updateTabItems(session, "tabs", "ex_liab")
+        updateTabsetPanel(session = session, inputId = "ex_liab_tabs", selected = "loan_tab")
+    })
+    
+    observeEvent(input$back2sheet,  {
+        updateTabItems(session, "tabs", "sheet")
+    })
+    
+    ### bank account balance ####
+    output$bank_balance <- renderUI({
+        numericInput(paste0('bank_account', 1), paste0('Balance ', 1),0)
+        # numericInput(varaible name,title, number )
+    })
+
+    ## reactive values
+    # store history as reactive values
+    num_bank <- reactiveValues(clicked = c(1)) # number of clicks
+    observeEvent(input$addbank, {
+        num_bank[['clicked']] <- 
+            c(num_bank[['clicked']],length(num_bank[['clicked']])+1)
+        insertUI("#addbank", "beforeBegin",
+                 ui= numericInput(paste0('bank_account', length(num_bank[['clicked']])), 
+                                  paste0('Balance ',length(num_bank[['clicked']]) ),0))
+    })
+    
+    ### Vehicle Value ####
+    output$vehicle_value <- renderUI({
+        numericInput(paste0('vehicle', 1), paste0('Vehicle ', 1),0)
+        # numericInput(varaible name,title, number )
+    })
+    
+    num_vehicle <- reactiveValues(clicked = c(1)) # number of clicks
+    observeEvent(input$addvehicle, {
+        num_vehicle[['clicked']] <- 
+            c(num_vehicle[['clicked']],length(num_vehicle[['clicked']])+1)
+        insertUI("#addvehicle", "beforeBegin",
+                 ui= numericInput(paste0('vehicle', length(num_vehicle[['clicked']])), 
+                                  paste0('Vehicle ',length(num_vehicle[['clicked']]) ),0))
+    })
+    ### credit card balance ####
+    output$credit_card_balance <- renderUI({
+        numericInput(paste0('credit_card', 1), paste0('Balance ', 1),0)
+        # numericInput(varaible name,title, number )
+    })
+
+    num_credit_card <- reactiveValues(clicked = c(1)) # number of clicks
+    observeEvent(input$addcreditcard, {
+        num_credit_card[['clicked']] <- 
+            c(num_credit_card[['clicked']],length(num_credit_card[['clicked']])+1)
+        insertUI("#addcreditcard", "beforeBegin",
+                 ui= numericInput(paste0('credit_card', length(num_credit_card[['clicked']])), 
+                                  paste0('Balance ',length(num_credit_card[['clicked']]) ),0))
+    })
+    
+    
+    ### loans #####
+    output$loan_balance <- renderUI({
+        numericInput(paste0('loan', 1), paste0('Loan ', 1),0)
+        # numericInput(varaible name,title, number )
+    })
+    
+    num_loan <- reactiveValues(clicked = c(1)) # number of clicks
+    observeEvent(input$addloan, {
+        num_loan[['clicked']] <- 
+            c(num_loan[['clicked']],length(num_loan[['clicked']])+1)
+        insertUI("#addloan", "beforeBegin",
+                 ui= numericInput(paste0('loan_account', length(num_loan[['clicked']])), 
+                                  paste0('Loan ',length(num_loan[['clicked']]) ),0))
+    })
+    
+    
+    
+    
+    ### subtotal and total output ####
     # functions to calculate ...
     total_fin_asset <- 30
     total_ex_liab <- 40
--- RetirementPlan/ui.R
+++ RetirementPlan/ui.R
@@ -12,6 +12,8 @@ sidebar <- dashboardSidebar(
                  menuSubItem("Balance Sheet", tabName = "sheet"),
                  menuSubItem("Financial Assets", tabName = "asset"),
                  menuSubItem("Explicit Liablility", tabName = "ex_liab"),
+                 menuSubItem("Human Capital", tabName = "hum_cap"),
+                 menuSubItem("Implicit Liablility", tabName = "im_liab"),
                  menuSubItem("Saving and Consumption", tabName = "suggestion")),
         menuItem("Home Mortgage Financing", tabName = "house", icon = icon("home")),
         menuItem("Life and Health Insurance", tabName = "insurance", icon = icon("heart")),
@@ -28,14 +30,22 @@ sheet_page <- tabItem(tabName = "sheet",
                           box(
                               title = tagList(shiny::icon("wallet"), "Financial Assets"),
                               id = "asset", background = "aqua",
+                              valueBoxOutput("AssetBox"),
                               actionButton('jump_bank_account', 'Bank account'),
-                              valueBoxOutput("AssetBox")
+                              actionButton('jump_investment', 'Investment'),
+                              actionButton('jump_vehicle', 'Vehicle'),
+                              actionButton('jump_house', 'Housing'),
+                              actionButton('jump_pension', 'Pension'),
+                              actionButton('jump_equity', 'equity'),
+                              actionButton('jump_otherasset', 'Other Assets')
                               ),
+                          
                           box(
                               title = tagList(shiny::icon("credit-card"), "Explicit Liabilities"),
                               id = "ex_liability", background = "maroon",
+                              valueBoxOutput("ExLiabBox"),
                               actionButton('jump_credit_card', 'Credit Card'),
-                              valueBoxOutput("ExLiabBox")
+                              actionButton('jump_loan', 'Loan')
                               ),
                       ),
                 
@@ -63,29 +73,73 @@ sheet_page <- tabItem(tabName = "sheet",
 
 #### Financial Asset Page ####
 asset_page <- tabItem(tabName = "asset",
-                             fluidRow(
-                             box(
-                                 title = tagList(shiny::icon("bank"), "Bank Account"),
-                                 id = "bank_account_balance", 
-                                 numericInput("bank_account", "Account Balance",
-                                          0))
-                             )
-                             
+                      tabsetPanel(
+                          id = "asset_tabs",
+                          tabPanel(
+                              value = "bank_tab",
+                              title = "Bank Account Balance",
+                              uiOutput("bank_balance"),
+                              actionButton("addbank", "Add"),
+                              actionButton("back2sheet", "Back")),
+                              
+                          tabPanel(
+                              value = "investment_tab",
+                              title = "Investments"),
+                          
+                          tabPanel(
+                              value = "vehicle_tab",
+                              title = "Vehicles",
+                              uiOutput("vehicle_value"),
+                              actionButton("addvehicle", "Add"),
+                              actionButton("back2sheet", "Back")),
+                          
+                          tabPanel(
+                              value = "house_tab",
+                              title = "Housing"),
+                    
+
+                          tabPanel(
+                              value = "pension_tab",
+                              title = "Pension"),
+                          
+                          tabPanel(
+                              value = "equity_tab",
+                              title = "Equity"),
+                          
+                          tabPanel(
+                              value = "otherasset_tab",
+                              title = "Other Assets")
+                          )
 )
+                      
+
 
 #### Explicit Liability Page ####
 ex_liab_page <- tabItem(tabName = "ex_liab",
-                      fluidRow(
-                          box(
-                              title = tagList(shiny::icon("bank"), "Credit Card"),
-                              id = "CreditCards", 
-                              numericInput("credit_card", "Balance",
-                                           0))
-                      )
+                        tabsetPanel(
+                            id = "ex_liab_tabs",
+                            tabPanel(
+                                value = "credit_card_tab",
+                                title = "Credit Card Balance", 
+                                uiOutput("credit_card_balance"),
+                                actionButton("addcreditcard", "Add"),
+                                actionButton("back2sheet", "Back")),
+                            tabPanel(
+                                value = "loan_tab",
+                                title = "Loans", 
+                                uiOutput("loan_balance"),
+                                actionButton("addloan", "Add"),
+                                actionButton("back2sheet", "Back"))
+                        )
                       
 )
 
+#### Human Capital Page ####
+hum_cap_page <- tabItem(tabName = "hum_cap")
+                        
 
+#### Implicit Liability Page ####
+im_liab_page <- tabItem(tabName = "im_liab")
 
 #### dashboard body ####        
 body <- dashboardBody(
@@ -93,6 +147,8 @@ body <- dashboardBody(
         sheet_page,
         asset_page,
         ex_liab_page,
+        hum_cap_page,
+        im_liab_page,
         tabItem(tabName = "suggestion",
                 h2("Dashboard tab content")
         ),       
